5cbfb880ebd14ada08676ba4ec305c7bbe68cfc8
abl <ablay.yedilbayev@kaspi.kz>
2024-09-07 13:39:34 +0500

    Create notification

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+	companion object {
+		private const val CHANNEL_ID = "channel_id"
+		private const val CHANNEL_NAME = "channel_name"
+	}
+	private var id = 0
 
 	private val binding by lazy {
 		ActivityMainBinding.inflate(layoutInflater)
@@ -25,5 +36,27 @@ class MainActivity : AppCompatActivity() {
 		binding.simpleService.setOnClickListener {
 			startService(MyService.newIntent(this, 25))
 		}
+		binding.foregroundService.setOnClickListener {
+			showNotification()
+		}
+	}
+
+	private fun showNotification() {
+		val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			val notificationChannel = NotificationChannel(
+				CHANNEL_ID,
+				CHANNEL_NAME,
+				NotificationManager.IMPORTANCE_DEFAULT
+			)
+			notificationManager.createNotificationChannel(notificationChannel)
+		}
+
+		val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+			.setContentTitle("title")
+			.setContentText("text")
+			.setSmallIcon(R.drawable.ic_launcher_foreground)
+			.build()
+		notificationManager.notify(id++, notification)
 	}
 }
