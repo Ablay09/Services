e9934bfbe31c365c0b61574a5a366b72490dde54
abl <ablay.yedilbayev@kaspi.kz>
2024-09-08 13:58:31 +0500

    Start foreground service

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.services
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.services.databinding.ActivityMainBinding
@@ -15,10 +11,7 @@ import com.example.services.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
 	companion object {
-		private const val CHANNEL_ID = "channel_id"
-		private const val CHANNEL_NAME = "channel_name"
 	}
-	private var id = 0
 
 	private val binding by lazy {
 		ActivityMainBinding.inflate(layoutInflater)
@@ -37,26 +30,10 @@ class MainActivity : AppCompatActivity() {
 			startService(MyService.newIntent(this, 25))
 		}
 		binding.foregroundService.setOnClickListener {
-			showNotification()
-		}
-	}
-
-	private fun showNotification() {
-		val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-			val notificationChannel = NotificationChannel(
-				CHANNEL_ID,
-				CHANNEL_NAME,
-				NotificationManager.IMPORTANCE_DEFAULT
+			ContextCompat.startForegroundService(
+				this,
+				MyForegroundService.newIntent(this)
 			)
-			notificationManager.createNotificationChannel(notificationChannel)
 		}
-
-		val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-			.setContentTitle("title")
-			.setContentText("text")
-			.setSmallIcon(R.drawable.ic_launcher_foreground)
-			.build()
-		notificationManager.notify(id++, notification)
 	}
 }
