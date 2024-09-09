401005e5cdc778ec81937efe6e1774e88d2d9b26
abl <ablay.yedilbayev@gmail.com>
2024-09-09 08:35:07 +0500

    Create MyJobService.kt

---
+++ app/src/main/java/com/example/services/MyJobService.kt
@@ -0,0 +1,52 @@
+package com.example.services
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyJobService : JobService() {
+
+    companion object {
+        private const val TAG = "SERVICE_TAG"
+    }
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        coroutineScope.cancel()
+    }
+
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        log("onStartJob")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000L)
+                log("Timer: $i")
+            }
+            jobFinished(p0, true)
+        }
+
+        return true
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    private fun log(message: String) {
+        Log.d(TAG, "MyService: $message ")
+    }
+}
